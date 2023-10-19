988c71d3c03706be1f260ccfea9fb07aaf29c257
Deebiga19990 <deebigaece@gmail.com>
2023-10-19 01:29:10 -0400

    first commit

---
+++ 6OCT_Task1.py
@@ -0,0 +1,9 @@
+name=input("Enter the name\n")
+print(name)
+F_Name=input("Enter first name\n")
+L_Name=input("Enter last name\n")
+print(F_Name,L_Name,sep=" ",end="\n")
+U_Name=input("Enter user name\n")
+print("Welcome","<",U_Name,">","to the python class",sep=" ",end="\n")
+print("Welcome ","<",U_Name,">"," to the python class",sep="",end="\n")
+
