63436d7d721a3a2eadeb564881d9e751bb6c7eaf
Deebiga19990 <deebigaece@gmail.com>
2023-10-25 22:18:05 -0400

    first commit

---
+++ 18OCT_Task1.py
@@ -0,0 +1,9 @@
+#factorial
+number=int(input("Enter the number\n"))
+fact=1
+if(number==0):
+    print("factorial is not possible")
+else:
+    for number in range(1,number+1):
+        fact=fact*number
+print(f"factorial of {number} is {fact}")
